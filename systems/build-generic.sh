f92729f03884b067eed6897b915c25e64fea242e
Erik Hetzner <ehetzner@gmail.com>
2008-01-17 22:57:26 -0800

    cache tar.gz for faster package building

--- systems/build-generic.sh
+++ systems/build-generic.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 OLDDIR=`pwd`
 TMPDIR=`mktemp -d /tmp/pases.XXXXXXXXXX` || exit 1
+if [ ! -e ${OLDDIR}/${BASE}-${V}.tar.gz ] ; then
+    wget $URL -q
+fi
 cd ${TMPDIR}
-wget $URL -q
-tar xzf ${BASE}-${V}.tar.gz
-cp ${OLDDIR}/${BASE}.pasdef ${BASE}-${V}/
+tar xzf ${OLDDIR}/${BASE}-${V}.tar.gz
+cp ${OLDDIR}/${BASE}.pasdef ${TMPDIR}/${BASE}-${V}/
 cd ${TMPDIR}/${BASE}-${V}
-tar czf ../${BASE}-${V}.pases *
-mv ../${BASE}-${V}.pases ${OLDDIR}
+tar czf ${TMPDIR}/${BASE}-${V}.pases *
+mv ${TMPDIR}/${BASE}-${V}.pases ${OLDDIR}
 cd ${OLDDIR}
 rm -rf ${TMPDIR}
 echo "Build package from source at ${URL}: ${BASE}-${V}.pases"
